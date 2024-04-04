cc7bfd7d73670a220488cb967040ed5ca173b205
Константин Геворкян <konstantin.gevorkyan.personal@gmail.com>
2024-04-05 01:49:35 +0500

    Update to actual

--- source/main.cpp
+++ source/main.cpp
@@ -4,22 +4,24 @@
 
 #include <vector>
 
-#include "board.hpp"
+#include "board/board.hpp"
 #include "error/error.hpp"
-#include "settings.hpp"
-#include "rook.hpp"
-#include "utils/auto_cpu_timer.hpp"
+#include "figures/rook.hpp"
+#include "logger/logger.hpp"
+#include "settings/settings.hpp"
 
 
 int main(int argc, char **argv)
 {
-	tt_utils::auto_cpu_timer timer("main()"); 
+	tt_program::event_logger logger;
+
+	logger.start();
 
 	int res = error::to_int(error::errc::ok);
 
 	auto settings = tt_program::make_default_settings();
 	
-	tt_program::board_t board(settings.rook_count);
+	tt_program::board_t board(settings.rook_count, logger);
 	std::vector<std::thread> thread_pool;
 	std::vector<tt_program::rook_t> rook_pool;
 	
@@ -41,19 +43,20 @@ int main(int argc, char **argv)
 
 		thread_pool.emplace_back( std::thread(rook_moves) );
 	}
+//std::cout << "pre joint" << std::endl;
+	board.add_rook();
+//std::cout << "add_rook 0 " << std::endl;
+	board.start_game();
+
+	rook_pool[0].start_moves();
 
 	for(auto & thread : thread_pool)
 	{
 		thread.join();
 	}
+//std::cout << "after join" << std::endl;
 
-	board.add_rook();
-	board.start_game();
-
-	rook_pool[0].start_moves();
-
-
-	std::cout << "hello world" << std::endl;
+	logger.stop();
 
 	return res;
 }
